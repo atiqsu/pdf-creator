db3d45884c91907bdef101874244807a58a63e38
Memtoko <sales@memtoko.com>
2014-12-08 11:01:27 +0700

    Fix psr

--- src/PdfCreator.php
+++ src/PdfCreator.php
@@ -20,10 +20,10 @@ class PdfCreator implements PdfCreatorInterface
     /**
      * Create pdf from $url
      *
-     * @param  string $url    The url to load the resource html
-     * @param  string $path   The path to save pdf
-     * @param  \Closure|null $callback     A callback to call before create pdf
-     * @return void
+     * @param  string        $url      The url to load the resource html
+     * @param  string        $path     The path to save pdf
+     * @param  \Closure|null $callback A callback to call before create pdf
+     * @return int|boolean If can't create return false
      */
     public function url($url, $path, Closure $callback = null)
     {
@@ -35,16 +35,15 @@ class PdfCreator implements PdfCreatorInterface
     /**
      * Create pdf from a file
      *
-     * @param string $filePath Path to file to resource html
-     * @param string $path Path to save pdf file
+     * @param string        $filePath Path to file to resource html
+     * @param string        $path     Path to save pdf file
      * @param \Closure|null $callback The callback to configure the creation of pdf
+     * @return int|boolean If can't create return false
      */
     public function file($filePath, $path, Closure $callback = null)
     {
-        if(! file_exists($filePath))
-        {
+        if (! file_exists($filePath)) {
             throw new \Exception("$filePath not exists");
-
         }
 
         $html = file_get_contents($filePath);
@@ -55,16 +54,16 @@ class PdfCreator implements PdfCreatorInterface
     /**
      * Create pdf file from raw html
      *
-     * @param string $html The html to convert to pdf
-     * @param path $path The Path to save pdf file
+     * @param string        $html     The html to convert to pdf
+     * @param path          $path     The Path to save pdf file
      * @param \Closure|null $callback The callback to configure the creation of pdf
+     * @return int|boolean If can't create return false
      */
     public function raw($html, $path, Closure $callback = null)
     {
-        $dompdf = new DOMPDF;
+        $dompdf = new DOMPDF();
 
-        if(isset($callback))
-        {
+        if (isset($callback)) {
             call_user_func($callback, $dompdf);
         }
 
@@ -81,6 +80,6 @@ class PdfCreator implements PdfCreatorInterface
      */
     protected function getClient()
     {
-        return new Client;
+        return new Client();
     }
 }
--- src/PdfCreatorInterface.php
+++ src/PdfCreatorInterface.php
@@ -18,28 +18,30 @@ interface PdfCreatorInterface
     /**
      * Create pdf from $url
      *
-     * @param  string $url    The url to load the resource html
-     * @param  string $path   The path to save pdf
+     * @param  string        $url      The url to load the resource html
+     * @param  string        $path     The path to save pdf
      * @param  \Closure|null $callback The callback to configure the creation of pdf
-     * @return void
+     * @return int|boolean If can't create return false
      */
     public function url($url, $path, Closure $callback = null);
 
     /**
      * Create pdf from a file
      *
-     * @param string $filePath Path to file to resource html
-     * @param string $path Path to save pdf file
+     * @param string        $filePath Path to file to resource html
+     * @param string        $path     Path to save pdf file
      * @param \Closure|null $callback The callback to configure the creation of pdf
+     * @return int|boolean If can't create return false
      */
     public function file($filePath, $path, Closure $callback = null);
 
     /**
      * Create pdf file from raw html
      *
-     * @param string $html The html to convert to pdf
-     * @param path $path The Path to save pdf file
+     * @param string        $html     The html to convert to pdf
+     * @param path          $path     The Path to save pdf file
      * @param \Closure|null $callback The callback to configure the creation of pdf
+     * @return int|boolean If can't create return false
      */
     public function raw($html, $path, Closure $callback = null);
 }
